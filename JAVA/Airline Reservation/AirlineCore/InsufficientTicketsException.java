c10523ee1586e4808de0eee1a5a24f9c6945f298
Manasa0108 <87691553+Manasa0108@users.noreply.github.com>
2021-08-05 21:43:07 +0530

    Delete JAVA directory

--- JAVA/Airline Reservation/AirlineCore/InsufficientTicketsException.java
+++
@@ -1,7 +0,0 @@
-package AirlineCore;
-
-public class InsufficientTicketsException extends Exception{
-	public String toString(){
-		return " No Tickets Available! Please Check again later!\n";
-	}
-}
--- JAVA/Airline Reservation/AirlineCore/NoFlightsException.java
+++
@@ -1,7 +0,0 @@
-package AirlineCore;
-
-public class NoFlightsException extends Exception{
-	public String toString(){
-		return " No Flights Matching Requirements Found!\n";
-	}
-}
